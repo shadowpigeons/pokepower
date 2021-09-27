52296eefd09da7b7ce5d258d466e60a58c2bb332
Shadow Pigeons <heidispiceland@gmail.com>
2021-09-26 20:03:22 -0400

    we added support form mutiple pokemon, and enabled searching by name and the pokedex entry number.

--- pokepower.py
+++ pokepower.py
@@ -2,10 +2,19 @@
 
 import json
 from pathlib import Path
+import sys
 
 data = json.loads(Path("./data.json").read_text())
+key = sys.argv[1]
+value = sys.argv[2]
+for pokemon in data:
+    if "name" == key.lower():
+        if pokemon["name"] == value.lower():
+            print(pokemon)
+    if "entry_dex_number" == key.lower():
+        if pokemon["entry_dex_number"] == int(value.lower()):
+            print(pokemon)
 
-
-
-print('Bulbasaur: pokedex #1')
-print(json.dumps(data, indent=4))
+# print('Pokedex:')
+# print(json.dumps(data, indent=4))
+# print(sys.argv)
