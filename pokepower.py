a08a70f528bdce0aa7075e08b21d8791d8161a78
Shadow Pigeons <heidispiceland@gmail.com>
2021-09-27 12:45:30 -0400

    Add some more pokemon; print a message when the search yields no results.

--- pokepower.py
+++ pokepower.py
@@ -7,14 +7,21 @@ import sys
 data = json.loads(Path("./data.json").read_text())
 key = sys.argv[1]
 value = sys.argv[2]
+found = None
 for pokemon in data:
     if "name" == key.lower():
         if pokemon["name"] == value.lower():
-            print(pokemon)
+            #print(json.dumps(pokemon, indent=4))
+            found = pokemon
     if "entry_dex_number" == key.lower():
         if pokemon["entry_dex_number"] == int(value.lower()):
-            print(pokemon)
+            #print(json.dumps(pokemon, indent=4))
+            found = pokemon
 
+if found is not None:
+    print(json.dumps(found, indent=1))
+else:
+    print("Could not find this pokemon. You might have spelled it wrong, or we have not implemented it as of this moment.")
 # print('Pokedex:')
-# print(json.dumps(data, indent=4))
+# print(json.dumps(pokemon, indent=4))
 # print(sys.argv)
